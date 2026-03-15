bcc7e548e95e72c22c2ba40d504119ad78144d5a
Lakshyashetty <lakshyashetty2403@gmail.com>
2026-03-15 16:26:15 +0530

    "Branch 2 Creation"

---
+++ Lbs_git/src/day2/Program1.java
@@ -0,0 +1,13 @@
+package day2;
+
+public class Program1 {
+public static void main(String[] args) {
+	int b=2;
+	int p=2;
+	int result=1;
+			for (int i = 1; i <=p; i++) {
+				result=result*b;
+			}
+			System.out.println(" Total Power of Number is :"+result);
+}
+}
